23f6c57b6bd3a794060f99e253cba82e14d24ebe
lxazw <156243916+lxazw@users.noreply.github.com>
2024-01-12 07:23:42 -0800

    Mars exploration - Python3

---
+++ 3 Months Preparation Kit/Week 2/Mars exploration.py
@@ -0,0 +1,17 @@
+# I'm close to you. Look around you carefully.
+
+import sys
+
+S = input().strip()
+
+num_errors = 0
+
+for i, char in enumerate(S):
+    if i % 3 == 1:
+        if char != 'O':
+            num_errors += 1
+    else:
+        if char != 'S':
+            num_errors += 1
+  
+print(num_errors)
